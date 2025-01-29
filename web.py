7cfb3c164ed4edd1cd671ecf14b6d272923cf802
chuy-zip <andreschuy03@gmail.com>
2025-01-29 13:29:00 -0600

    buffer implemnts regex

--- web.py
+++ web.py
@@ -19,23 +19,22 @@ def cargar_buffer(entrada: list[str], inicio: int, tamano_buffer: int) -> (list[
     return buffer
 
 # Procesar y extraer lexemas del buffer
-def procesar_buffer(lexema, buffer):
+def procesar_buffer(lexema, buffer, prop_list):
     avance: int = 0
     for i in range(len(buffer)):
         avance += 1
         if buffer[i] == "eof":
-            print("Lexema procesado2: " + lexema)
-            return "eof", avance
-        elif buffer[i] == " ":
             print("Lexema procesado: " + lexema)
+            extraer_productos(lexema,prop_list)
+            return "eof", avance
+        elif buffer[i] == "\n":
+            print("Lexema procesado: " + lexema)
+            extraer_productos(lexema,prop_list)
             lexema = ""
         else:
             lexema += buffer[i]
 
     return lexema, avance
-
-
-
 
 # Abrir archivo HTML
 def cargar_html(archivo):
@@ -43,20 +42,30 @@ def cargar_html(archivo):
         return f.read()
 
 # Buscar productos y URLs de imágenes
-def extraer_productos(html):
+def extraer_productos(html_line, prop_list: list[str]):
    # Processing data using Regular Expressions.
    re_imagenes = r'<img class="img-fluid card-img-top image img-responsive" alt="item" src="(.*?)">'
    re_titulos = r'title="(.*?)"'
    re_precios = r'<h4 class="price float-end card-title pull-right">(\$\d+(?:\.\d+)?)<\/h4>'
    
-   img = re.findall(re_imagenes, html) 
-   titulo = re.findall(re_titulos, html) 
-   precio = re.findall(re_precios, html)
+   #Este es el orden del html para las caracteristicas de un producto
+   img:list[str] = re.findall(re_imagenes, html_line) 
+   precio:list[str] = re.findall(re_precios, html_line)
+   titulo:list[str] = re.findall(re_titulos, html_line) 
+
    print(img)
    print(titulo)
    print(precio)
 
-   return img, titulo, precio
+   if len(img) != 0:
+       prop_list.append(img[0])
+   
+   if len(precio) != 0:
+       prop_list.append(precio[0])
+
+   if len(titulo) != 0:
+       prop_list.append(titulo[0])
+
 
 # Exportar resultados a CSV
 def exportar_csv(productos, archivo_salida):
@@ -64,7 +73,6 @@ def exportar_csv(productos, archivo_salida):
         writer = csv.writer(csvfile)
         writer.writerow(['Nombre del Producto', 'URL de la Imagen'])
         writer.writerows(productos)
-
 
 html = cargar_html("test.html")
 #print(html)
@@ -77,9 +85,17 @@ lexema = ""
 entrada = list(html)
 print(len(entrada))
 
+# lista donde se van a guardar las propiedades. Al final la lista debería tener las propiedades en el siguiente orden
+# [img1,titulo1,precio1,img2,titulo2,precio3,....]
+prop_list = []
+
 while lexema != "eof":
    buffer = cargar_buffer(entrada, inicio, tamano_buffer)
    print(buffer)
-   lexema, avance = procesar_buffer(lexema, buffer)
+   lexema, avance = procesar_buffer(lexema, buffer, prop_list)
 
    inicio += avance
+
+print("\nFinal: ", prop_list)
+
+#Ahora hace falta la parte del ouput
