cf051698f3b46bbfecb9771005c8712d604237d7
chuy-zip <andreschuy03@gmail.com>
2025-01-29 12:21:02 -0600

    added regex

--- web.py
+++ web.py
@@ -1,30 +1,36 @@
 # Importing the required libraries.
-import requests
-from bs4 import BeautifulSoup
 import re
-from pprint import pprint
+import csv
 
-# Requesting the HTML from the target website.
-url = "https://sandbox.oxylabs.io/products"
-page = requests.get(url)
+# Abrir archivo HTML
+def cargar_html(archivo):
+    with open(archivo, "r", encoding="utf-8") as f:
+        return f.read()
 
-# Selecting data.
-soup = BeautifulSoup(page.content, "html.parser")
-products = soup.find_all("div", class_="product-card")
+# Buscar productos y URLs de imágenes
+def extraer_productos(html):
+    regex_nombre = r"<h2>(.*?)</h2>"
+    regex_imagen = r"<img src='(.*?)' alt='product-image'/>"
+    nombres = re.findall(regex_nombre, html)
+    imagenes = re.findall(regex_imagen, html)
+    return zip(nombres, imagenes)
+
+# Exportar resultados a CSV
+def exportar_csv(productos, archivo_salida):
+    with open(archivo_salida, "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Nombre del Producto', 'URL de la Imagen'])
+        writer.writerows(productos)
+
+
+html = cargar_html("test.html")
+#print(html)
 
 # Processing data using Regular Expressions.
-re_titles = r'class="title css-7u5e79 eag3qlw7">(.*?)<\/h4>'
-re_prices = r'class="price-wrapper css-li4v8k eag3qlw4">(.*?)<\/div>'
+re_imagenes = r'<img class="img-fluid card-img-top image img-responsive" alt="item" src="(.*?)">'
+re_titulos = r'title="(.*?)"'
+re_precios = r'<h4 class="price float-end card-title pull-right">(\$\d+(?:\.\d+)?)<\/h4>'
 
-data = []
-
-for product in products:
-   product_html = str(product)
-   title = re.findall(re_titles, product_html)
-   price = re.findall(re_prices, product_html)
-   data.append((title, price))
-
-# Saving the output.
-with open("output.txt", "w", encoding="utf-8") as f:
-   for title, price in data:
-       f.write(f"{title}\t{price}\n")
+print(re.findall(re_imagenes, html))
+print(re.findall(re_titulos, html))
+print(re.findall(re_precios, html))
